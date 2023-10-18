c45a70dba4d3433fdcc44d8fc38868dc57ea6ae9
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-10-18 20:10:20 +0600

    added routes and pages

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className="text-5xl font-bold font-heading">404</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
